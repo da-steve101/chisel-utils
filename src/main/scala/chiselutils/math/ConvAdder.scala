e450a9837682408e6f52421b1001f62e58eaeb10
Stephen Tridgell <stephen@tridgell.net>
2016-09-23 17:30:33 +1000

    Conv Adder working, tested for multiple images and filters

--- src/main/scala/chiselutils/math/ConvAdder.scala
+++ src/main/scala/chiselutils/math/ConvAdder.scala
@@ -15,10 +15,9 @@ class ConvAdder( filterSize : (Int, Int, Int), imgSize : (Int, Int) ) extends Mo
     val multIn = Vec( filterSize._1 * filterSize._2, Vec( filterSize._3, Fixed( INPUT, 16, 8 ) ) )
     val validOut = Bool( OUTPUT )
     val convOut = Vec( filterSize._3, Fixed( OUTPUT, 16, 8 ) )
-    val rowAddOut = Vec( filterSize._1, Vec( filterSize._3, Fixed( OUTPUT, 16, 8 ) ) )
   }
 
-  val rowSumLat = 3 * ( filterSize._2 / 2 )
+  val rowSumLat = filterSize._2 + 1 + ( (filterSize._2 - 2) / 2 )
   val colSumLat = imgSize._2 * ( filterSize._1 / 2 ) + 2
 
   def latency : Int = rowSumLat + colSumLat
@@ -65,54 +64,44 @@ class ConvAdder( filterSize : (Int, Int, Int), imgSize : (Int, Int) ) extends Mo
 
   val noRowMuxs = muxConds.size
 
-  for ( fIdx <- 0 until filterSize._1 )
-    io.rowAddOut( fIdx )( 0 ) := rowAdder.last( fIdx )( 0 )
-
   val colAdder = ArrayBuffer[ List[ Fixed ] ]()
-  colAdder += rowAdder.last( 0 ).map( ShiftRegister( _, imgSize._2 ) )
+  colAdder += rowAdder.last( 0 ).map( ShiftRegister( _, 2 ) )
 
   for ( cIdx <- 1 until filterSize._1 ) {
     val inputSelected = rowAdder.last( cIdx )
+    val srIn = colAdder.last.map( ShiftRegister( _, imgSize._2 - 2 ) )
+
     val inputDelayed = {
       if ( cIdx <= filterSize._1 / 2 )
         inputSelected.map( RegNext(_) )
       else
-        colAdder.last.map( RegNext(_) )
+        srIn.map( RegNext( _ ) )
     }
-    val adder = ( colAdder.last zip inputSelected ).map( ab => {
+    val adder = ( srIn zip inputSelected ).map( ab => {
       RegNext( ab._1 + ab._2 )
     })
-
-    printf( "adder( " + cIdx + " ) = ( %d, %d ) in and %d out\n", colAdder.last(0), inputSelected(0), adder(0) )
 
     muxConds += Bool()
     val muxOut = ( adder zip inputDelayed ).map( ab => {
       RegNext( Mux( muxConds.last, ab._2, ab._1 ) )
     })
 
-    printf( "muxOut( " + cIdx + " ) = ( %d, %d ) in and %d out\n", adder(0), inputDelayed(0), muxOut(0) )
-    if ( cIdx != filterSize._1 - 1 )
-      colAdder += muxOut.map( ShiftRegister( _, imgSize._2 - 2 ) )
-    else
-      colAdder += muxOut
+    colAdder += muxOut
   }
 
   val validSR = List( io.validIn ) ++ List.fill( latency ) { RegInit( Bool(false) ) }
   for ( vIdx <- 1 until validSR.size )
     validSR( vIdx ) := validSR( vIdx - 1 )
 
-  var lastInitVal = 0
-
   for ( mIdx <- 0 until noRowMuxs ) {
     val initialVal = {
       if ( mIdx < filterSize._2/2 )
-        ( imgSize._2 - mIdx - 1 ) % imgSize._2
+        ( 2*imgSize._2 - mIdx - 1 ) % imgSize._2
       else {
         val cutoff = (filterSize._2 / 2)
-        ( imgSize._2 - cutoff - 2*( mIdx - cutoff + 1) ) % imgSize._2
+        ( 2*imgSize._2 - cutoff - 2*( mIdx - cutoff + 1) ) % imgSize._2
       }
     }
-    lastInitVal = initialVal
 
     val cntr = RegInit( UInt( initialVal, log2Up( imgSize._2 ) ) )
     when ( io.validIn ) {
@@ -126,18 +115,18 @@ class ConvAdder( filterSize : (Int, Int, Int), imgSize : (Int, Int) ) extends Mo
     if ( mIdx >= filterSize._2 / 2 )
       muxConds( mIdx ) := { cntr >= UInt( imgSize._2 - 1 - ( mIdx - (filterSize._2 / 2 ) ) ) }
     else
-      muxConds( mIdx ) := { cntr <= UInt( mIdx ) }
+      muxConds( mIdx ) := { cntr <= UInt( 0 ) }
   }
 
   for ( cIdx <- 0 until muxConds.size - noRowMuxs ) {
     val mIdx = cIdx + noRowMuxs
 
-    val initialVal = ( imgSize._2 + lastInitVal - 2 ) % imgSize._2
-    val initialRow = ( imgSize._1 - 1 - {
+    val initialVal = ( 2*imgSize._2 - rowSumLat - 1 ) % imgSize._2
+    val initialRow = ( imgSize._1 - 1 - ( rowSumLat / imgSize._2 ) - {
       if ( cIdx < filterSize._1 / 2 )
         0
       else
-        cIdx // - ( filterSize._1 / 2 )
+        cIdx - ( filterSize._1 / 2 ) + 1
     } ) % imgSize._1
 
     val cntr = RegInit( UInt( initialVal, log2Up( imgSize._2 ) ))
@@ -153,60 +142,12 @@ class ConvAdder( filterSize : (Int, Int, Int), imgSize : (Int, Int) ) extends Mo
       }
     }
 
-    printf( "cntr( " + mIdx + " ) = %x\n", cntr )
-    printf( "rowCntr( " + mIdx + " ) = %x\n", rowCntr )
-
     // if below filter
     if ( cIdx >= filterSize._1 / 2 )
       muxConds( mIdx ) := { rowCntr >= UInt( imgSize._1 - 1 - ( cIdx - (filterSize._1 / 2 ) ) ) }
     else
-      muxConds( mIdx ) := { rowCntr <= UInt( cIdx ) }
+      muxConds( mIdx ) := { rowCntr <= UInt( 0 ) }
   }
-
-  /*
-   for ( mIdx <- 0 until muxConds.size ) {
-     if ( mIdx < filterSize._2 - 1 ) {
-      // row adder muxes
-      val validIdx = {
-        if ( mIdx < filterSize._2 / 2 )
-          mIdx
-        else {
-          val lastDelay = ( filterSize._2 / 2 ) - 1
-          lastDelay + 2*( mIdx - lastDelay )
-        }
-      }
-      val cntr = Counter( validSR( validIdx ), imgSize._2 )
-      printf( "cntr( " + mIdx + " ) = %x\n", cntr._1 )
-      val isRightOfCntr = { mIdx >= ( filterSize._2 / 2 ) }
-      if ( isRightOfCntr )
-        muxConds( mIdx ) := { cntr._1 >= UInt( imgSize._2 - ( imgSize._2 / 2 ) ) }
-      else
-        muxConds( mIdx ) := { cntr._1 <= UInt( imgSize._2 / 2 ) }
-
-    } else {
-      // col adder muxes
-      val lastIdx = filterSize._2  - 2
-      val noMuxRow = lastIdx + 1
-      val lastDelay = ( filterSize._2 / 2 ) - 1
-      val startIdx = lastDelay + 2 * ( lastIdx - lastDelay ) + 1
-      val diffIdx = mIdx - filterSize._2 + 1
-      val fromInput = diffIdx < ( filterSize._2 / 2 )
-      val latPos = {
-        if ( fromInput )
-          startIdx
-        else
-          startIdx + imgSize._2 * ( diffIdx - ( filterSize._2 / 2 ) + 1 )
-      }
-      val cntr = Counter( validSR( latPos ), imgSize._2 )
-      printf( "cntr( " + mIdx + " ) = %x\n", cntr._1 )
-      val isBelowOfCntr = { mIdx - noMuxRow >= ( filterSize._2 / 2 ) }
-      if ( isBelowOfCntr )
-        muxConds( mIdx ) := { cntr._1 >= UInt( imgSize._2 - ( imgSize._2 / 2 ) ) }
-      else
-        muxConds( mIdx ) := { cntr._1 <= UInt( imgSize._2 / 2 ) }
-    }
-  }
-*/
 
   io.convOut := Vec( colAdder.last )
   io.validOut := validSR.last
--- src/test/scala/ConvAdderSuite.scala
+++ src/test/scala/ConvAdderSuite.scala
@@ -10,18 +10,22 @@ class ConvAdderSuite extends TestSuite {
   val myRand = new Random
   val fracWidth = 8
 
+  /** Generate a random matrix
+    */
   def genMat( rows : Int, cols : Int ) : List[List[Int]] = {
     List.fill( rows ) { List.fill( cols ) { myRand.nextInt( 512 ) - 256 } }
   }
 
+  /** Compute the convolution with a filter and image
+    */
   def computeConv( filter : List[List[Int]], img : List[List[Int]] ) : List[List[Int]] = {
     // each individual number
     val convLocs = ( 0 until img.size ).map( rowIdx => {
       ( 0 until img(rowIdx).size ).map( colIdx => {
         ( 0 until filter.size ).map( fRow => {
           ( 0 until filter(fRow).size ).map( fCol => {
-            val imgRow = rowIdx + fRow - 1
-            val imgCol = colIdx + fCol - 1
+            val imgRow = rowIdx + fRow - ( filter.size / 2 )
+            val imgCol = colIdx + fCol - ( filter( 0 ).size / 2 )
             if ( imgRow >= 0 && imgRow < img.size && imgCol >= 0 && imgCol < img( imgRow ).size )
               filter( fRow )( fCol )*img( imgRow )( imgCol ) >> fracWidth
             else
@@ -32,62 +36,82 @@ class ConvAdderSuite extends TestSuite {
     }).toList
 
     val convRes = convLocs.map( a => a.map( b => b.map( _.reduce( _ + _ )).reduce( _ + _ ) ) )
-    // val rowSums = convLocs.map( a => a.map( b => b.map( _.reduce( _ + _ )) ) )
 
     convRes
   }
 
-  class ConvAdderTester( c : ConvAdder, filter : List[List[Int]],
-    img : List[List[Int]] ) extends Tester( c ) {
-    val imgSize = ( img.size, img(0).size )
-    val filterSize = ( filter.size, filter(0).size, 1 )
-    val noImg = 1
-    val noPixel = noImg * imgSize._1 * imgSize._2
+  class ConvAdderTester( c : ConvAdder, filterSize : ( Int, Int, Int ),
+    imgSize : ( Int, Int ) ) extends Tester( c ) {
 
-    val convRes = computeConv( filter, img )
+    val noImg = 2
+    val imgPixels = imgSize._1 * imgSize._2
+    val filterPixels = filterSize._1*filterSize._2
+    val noPixel = noImg * imgPixels
+
+    val filters = ( 0 until filterSize._3 ).map( x => genMat( filterSize._1, filterSize._2 ) )
+    println( "filters = " + filters )
+
+    val imgs = ( 0 until noImg ).map( x => genMat( imgSize._1, imgSize._2 ) ).toList
+    println( "imgs = " + imgs )
+
+    val convResAry = imgs.map( img => filters.map( filter => computeConv( filter, img ) ) )
+
+    println( "convResAry = " + convResAry )
+    println( "latency = " + c.latency )
 
     poke( c.io.validIn, true )
 
     for ( pIdx <- 0 until noPixel ) {
+      val img = imgs( pIdx / imgPixels )
+
       val imgRow = ( pIdx/imgSize._2 ) % imgSize._1
       val imgCol = pIdx % imgSize._2
 
-      val multIn = filter.map( fRow => fRow.map( f => {
-        f*img( imgRow )( imgCol ) >> fracWidth
-      })).reduce( _ ++ _ ).map( x => Array( BigInt(x) ) ).toArray
+      val filterInputs = filters.map( filter => {
+        filter.map( fRow => {
+          fRow.map( f => {
+            f*img( imgRow )( imgCol ) >> fracWidth
+          })
+        }).reduce( _ ++ _ ).map( x => Array( BigInt(x) ) )
+      })
 
-      for ( idx <- 0 until filterSize._1*filterSize._2 )
+      val multIn = ( 0 until filterPixels ).map( idx => filterInputs.map( f => f(idx) ).reduce( _ ++ _ ) )
+
+      for ( idx <- 0 until filterPixels )
         poke( c.io.multIn(idx), multIn(idx) )
-      step(1)
 
       if ( c.latency <= pIdx ) {
+        val imgIdx = ( pIdx - c.latency ) / imgPixels
+        val convRes = convResAry( imgIdx )
         val convRow = ( (pIdx - c.latency)/imgSize._2 ) % imgSize._1
         val convCol = ( pIdx - c.latency ) % imgSize._2
-        expect( c.io.convOut, Array( BigInt(convRes( convRow )( convCol )) ) )
+        println( "test result at ( " + imgIdx + ", " + convRow + ", " + convCol + " )" )
+        expect( c.io.convOut, ( 0 until filterSize._3 ).map( fIdx => BigInt( convRes( fIdx )( convRow )( convCol ) ) ).toArray )
       }
-    }
-    for ( pIdx <- ( noPixel - c.latency) until noPixel ) {
-      val convRow = ( pIdx/imgSize._2 ) % imgSize._1
-      val convCol = pIdx % imgSize._2
       step(1)
-      expect( c.io.convOut, Array( BigInt(convRes( convRow )( convCol )) ) )
+    }
+    for ( pIdx <- ( noPixel - c.latency ) until noPixel ) {
+      if ( pIdx >= 0 ) {
+        val imgIdx = pIdx / imgPixels
+        val convRes = convResAry( imgIdx )
+        val convRow = ( pIdx/imgSize._2 ) % imgSize._1
+        val convCol = pIdx % imgSize._2
+        println( "test result at ( " + imgIdx + ", " + convRow + ", " + convCol + " )" )
+        expect( c.io.convOut, ( 0 until filterSize._3 ).map( fIdx => BigInt( convRes( fIdx )( convRow )( convCol ) ) ).toArray )
+      }
+      step(1)
     }
   }
 
 
-  @Test def conv_3x3_5x5_Test {
-    for ( filterLen <- List(5) ) {
-      for ( imgLen <- List(7) ) {
+  @Test def convTest {
+    for ( filterLen <- List(3, 5, 7, 9) ) {
+      for ( imgLen <- ( filterLen + 2 until 21 by 2 ) ) {
         val imgSize = ( imgLen, imgLen )
-        val filterSize = ( filterLen, filterLen, 1 )
-        val img = genMat( imgSize._1, imgSize._2 )
-        val filter = genMat( filterSize._1, filterSize._2 )
-
-        println( "img = " + img )
-        println( "filter = " + filter )
+        val filterSize = ( filterLen, filterLen, 15 )
 
         chiselMainTest( Array("--genHarness", "--compile", "--test", "--vcd", "--backend", "c"),
-          () => Module( new ConvAdder( filterSize, imgSize ) ) ) { c => new ConvAdderTester( c, filter, img ) }
+          () => Module( new ConvAdder( filterSize, imgSize ) ) ) { c => new ConvAdderTester( c, filterSize, imgSize ) }
       }
     }
   }
