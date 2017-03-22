e48a2fe4503dcc4ad5fe042bca78bd5de306ec0f
Stephen Tridgell <stephen@tridgell.net>
2017-03-22 11:34:38 +1100

    Simplify the NodeSet data structure as another look at performance indiciates it does not improve anything

--- src/main/scala/chiselutils/algorithms/NodeSet.scala
+++ src/main/scala/chiselutils/algorithms/NodeSet.scala
@@ -7,68 +7,29 @@ package chiselutils.algorithms
 class NodeSet( nodesIn : Set[Node] ) {
   private val myTRand = java.util.concurrent.ThreadLocalRandom.current()
 
-  private var idxAvailable = List[Int]()
-  // a sorted vector
-  private var nodes = nodesIn.zipWithIndex.map( n => {
-    n._1.hashIdx = n._2
-    n._1
-  }).toVector
-
-  private def getIdx( n : Node ) : Int = synchronized {
-    var min = 0
-    var max = nodes.size - 1
-    val nHash = n.hashIdx
-    while ( min <= max ) {
-      val mid = ( min + max ) >> 1
-      if ( nodes( mid ).hashIdx == nHash )
-        return mid
-      if ( nHash < nodes( mid ).hashIdx )
-        max = mid - 1
-      else
-        min = mid + 1
-    }
-    min
-  }
-
-  private def verifySorted( s : String ) = synchronized {
-    for ( i <- 1 until nodes.size )
-      assert( nodes( i - 1 ).hashIdx < nodes( i ).hashIdx,  "Not sorted: " + s )
-  }
-
+  private val nodes = scala.collection.mutable.Set[Node]() ++ nodesIn
 
   def randomNode() : ( Node, Boolean ) = synchronized {
     val nRand = myTRand.nextInt(0, nodes.size)
-    val node = nodes( nRand )
+    val node = nodes.iterator.drop( nRand ).next
     val nodeLock = node.lockNode()
     ( node, nodeLock )
   }
 
   def remove( n : Node ) : Unit = synchronized {
     // get its index
-    val idx = getIdx( n )
-    assert( n == nodes( idx ), "Trying to remove bad node" )
-    nodes = nodes.patch( idx, Nil, 1 ) // this is constant right???
-    idxAvailable = n.hashIdx :: idxAvailable
+    nodes -= n
   }
 
   def add( n : Node ) : Unit = synchronized {
     // find where it should be put
-    if ( idxAvailable.isEmpty )
-      n.hashIdx = nodes.size
-    else {
-      n.hashIdx = idxAvailable.head
-      idxAvailable = idxAvailable.tail
-    }
-    val idx = getIdx( n )
-    assert( n != nodes( math.min( idx, nodes.size - 1 ) ), "Trying to add dup node" )
-    nodes = nodes.patch( idx, Vector(n), 0 )
+    nodes += n
   }
 
   def size() = synchronized { nodes.size }
 
   def contains( n : Node ) : Boolean = synchronized {
-    val idx = getIdx( n )
-    nodes( idx ) == n
+    nodes.contains( n )
   }
 
   def -=( n : Node ) = { remove(n) }
